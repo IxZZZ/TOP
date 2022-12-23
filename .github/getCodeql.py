6ae5257a8273bc50418a2df4b0aa062b6ac562e3
Sourcery AI <>
2022-12-23 16:16:37 +0000

    'Refactored by Sourcery'

--- .github/getCodeql.py
+++ .github/getCodeql.py
@@ -10,7 +10,7 @@ def log(s):
     xOut+=[s]
 
 cmpfun = lambda x:x["stargazers_count"]
-for i in range(0,10):
+for _ in range(10):
     log("## Codeql")
     r = requests.get('https://api.github.com/search/repositories?l=CodeQL&o=desc&q=codeql&s=stars&type=Repositories')
     a = json.loads(r.text)
@@ -25,17 +25,15 @@ for i in range(0,10):
             # print(json.dumps(x))
             # if x:
                 szDes = x["description"]
-                if None == szDes:
+                if szDes is None:
                     szDes = ""
                 log("|" +"|".join([str(x["stargazers_count"]),x["name"],x["html_url"],szDes])+"|")
             except Exception as e:
                 print(e)
-                pass
-                # print(x)
-            # break
+                            # print(x)
+                    # break
     break
-if 0 < len(xOut):
-    f11 = open("Top_Codqql1.md","wb")
-    f11.write("\n".join(xOut).encode('utf-8'))
-    f11.close()
+if len(xOut) > 0:
+    with open("Top_Codqql1.md","wb") as f11:
+        f11.write("\n".join(xOut).encode('utf-8'))
 # ?q=user%3Ahktalent&type=Repositories')
